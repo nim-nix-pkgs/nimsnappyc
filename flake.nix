3317b75973b60886f1cf756dc717abc7a5ecd830
hugosenari <hugosenari@gmail.com>
2022-03-12 04:31:51 -0300

    chore: re index nimsnappyc

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Wrapper for the Snappy-C compression library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimsnappyc-master".url = "path:./master";
+  inputs."nimsnappyc-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimsnappyc-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Wrapper for the Snappy-C compression library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimsnappyc-master.flake = false;
+  inputs.src-nimsnappyc-master.owner = "NimCompression";
+  inputs.src-nimsnappyc-master.ref   = "refs/heads/master";
+  inputs.src-nimsnappyc-master.repo  = "nimsnappyc";
+  inputs.src-nimsnappyc-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimsnappyc-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimsnappyc-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
